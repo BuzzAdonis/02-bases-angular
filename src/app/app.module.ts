94bb53403c6fa75825585b047678353e45d37b50
Adonis A. Severino <adonisalexander@hotmail.com>
2022-05-03 23:15:05 -0400

    El refuerzo en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{HttpClientModule} from'@angular/common/http';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './component/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //mover a Pages Module
+    //AboutComponent,
+    //ContactComponent,
+    //HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
